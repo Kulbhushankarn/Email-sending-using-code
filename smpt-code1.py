4c522158c345e46af7ad5650094a2cb2d19bd36a
Kulbhushan Karn <67707132+Kulbhushankarn@users.noreply.github.com>
2021-07-31 21:29:44 +0530

    Create smpt-code1.py

---
+++ smpt-code1.py
@@ -0,0 +1,13 @@
+import smtplib
+
+to=input("Enter To Email-id")
+sub=input("Enter subject")
+Meg=input("Enter Message")
+
+s=smtplib.SMTP('smtp.gmail.com',587)
+s.starttls()
+s.login("karnkulbhushan@gmail.com","rahulkarn")
+
+mymessage="subject:{}\n\n{}".format(sub,Meg)
+s.sendmail("karnkulbhushan@gmail.com",to,mymessage)
+print("Mail Send Successfully")
